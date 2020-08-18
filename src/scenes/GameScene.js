02764125e384ea73959367f1d23f6903de778541
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-18 21:44:30 +0530

    Change background Image

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,18 @@
-export default class GameScene extends Phaser.Scene {
+const backgroundRepeat = (scene, w, h, text, speed, s1, s2, o1, o2) => {
+  const screenWidth = scene.textures.get(text).getSourceImage().width
+  const totalWidth = scene.scale.width * 10
+  const count = Math.ceil(totalWidth / screenWidth) * speed
+  let x = 0;
+
+  for (let i = 0; i < count; i++) {
+    scene.add.image(w + x, h, text).setOrigin(o1, o2).setScrollFactor(speed).setScale(s1, s2)
+    x += scene.scale.width
+  }
+}
+
+export default class ParallaxScene extends Phaser.Scene {
   constructor() {
-    super('game-scene')
+    super('parallax-scene')
   }
 
   preload() {
@@ -20,52 +32,42 @@ export default class GameScene extends Phaser.Scene {
 
   }
 
-  backgroundRepeat(scene, w, h, count, text, speed, o1, o2, s1, s2) {
-    let x = 0;
-
-    for (let i = 0; i < count; i++) {
-      scene.add.image(w + x, h, text).setOrigin(o1, o2).setScrollFactor(speed).setScale(s1, s2)
-      x += scene.scale.width
-    }
-  }
-
   create() {
     const height = this.scale.height
     const width = this.scale.width
 
-    this.add.image(width * 0.5, height * 0.5, 'sky')
-      .setScrollFactor(0)
+    this.add.image(width * 0.5, height * 0.3, 'sky')
+      .setScrollFactor(0).setScale(0.8, 0.7)
 
-    this.mountain = backgroundRepeat(this, 0, height, 2, 'mountain', 0.25, 0.5, 0.5, 0, 1)
+    this.mountain = backgroundRepeat(this, 0, height, 'mountain', 0.25, 0.5, 0.5, 0, 1)
 
-    this.grass2 = backgroundRepeat(this, width / 2.4, height / 1.5, 2, 'grass2', 0.5, 0.4, 0.4)
+    this.grass2 = backgroundRepeat(this, width / 2.4, height / 1.5, 'grass2', 0.5, 0.4, 0.4)
 
-    this.grass1 = backgroundRepeat(this, width / 7.5, height / 1.5, 2, 'grass3', 0.5, 0.4, 0.4)
+    this.grass1 = backgroundRepeat(this, width / 7.5, height / 1.5, 'grass3', 0.5, 0.4, 0.4)
 
-    this.grass3 = backgroundRepeat(this, width / 1.3, height / 1.5, 2, 'grass1', 0.5, 0.4, 0.4)
+    this.grass3 = backgroundRepeat(this, width / 1.3, height / 1.5, 'grass1', 0.5, 0.4, 0.4)
 
-    this.ground = backgroundRepeat(this, 0, height / 1.1, 2, 'ground', 0.75, 0.5, 0.5, 0, 1)
+    this.ground = backgroundRepeat(this, 0, height / 1.1, 'ground', 0.75, 0.5, 0.5, 0, 1)
 
-    this.tree1 = backgroundRepeat(this, width / 5, height / 1.8, 2, 'tree', 0.75, 0.5, 0.5)
+    this.tree1 = backgroundRepeat(this, width / 5, height / 1.8, 'tree', 0.75, 0.5, 0.5)
 
-    this.tree2 = backgroundRepeat(this, width / 1.3, height / 1.6, 2, 'tree', 0.75, 0.35, 0.35)
+    this.tree2 = backgroundRepeat(this, width / 1.3, height / 1.6, 'tree', 0.75, 0.35, 0.35)
 
-    this.rock1 = backgroundRepeat(this, width / 1.8, height / 1.3, 2, 'rock2', 0.75, 0.4, 0.4)
+    this.rock1 = backgroundRepeat(this, width / 1.8, height / 1.3, 'rock2', 0.75, 0.4, 0.4)
 
-    this.rock2 = backgroundRepeat(this, width / 3.5, height / 1.3, 2, 'rock3', 0.75, 0.4, 0.4)
+    this.rock2 = backgroundRepeat(this, width / 3.5, height / 1.3, 'rock3', 0.75, 0.4, 0.4)
 
-    this.rock3 = backgroundRepeat(this, width / 1.1, height / 1.3, 2, 'rock1', 0.75, 0.4, 0.4)
+    this.rock3 = backgroundRepeat(this, width / 1.1, height / 1.3, 'rock1', 0.75, 0.4, 0.4)
 
-    this.flower1 = backgroundRepeat(this, width / 1.7, height / 1.2, 2, 'flower1', 0.75, 0.4, 0.4)
+    this.flower1 = backgroundRepeat(this, width / 1.7, height / 1.2, 'flower1', 0.75, 0.4, 0.4)
 
-    this.flower2 = backgroundRepeat(this, width / 2.5, height / 1.3, 2, 'flower2', 0.75, 0.4, 0.4)
+    this.flower2 = backgroundRepeat(this, width / 2.5, height / 1.3, 'flower2', 0.75, 0.4, 0.4)
 
-    this.ground2 = backgroundRepeat(this, 0, height, 2, 'ground2', 1.25, 0.45, 0.45, 0, 1)
+    this.ground2 = backgroundRepeat(this, 0, height, 'ground2', 1.25, 0.45, 0.45, 0, 1)
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.cameras.main.setBounds(0, 0, width * 3, height)
   }
-
 
   update() {
     const speed = 3
@@ -78,6 +80,5 @@ export default class GameScene extends Phaser.Scene {
       cam.scrollX += speed
     }
   }
-  
-}
 
+}
