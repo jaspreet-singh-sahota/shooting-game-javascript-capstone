70fdcdcd4386342a352e03254104afa671764fb0
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-18 21:45:55 +0530

    Add clouds

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -10,9 +10,9 @@ const backgroundRepeat = (scene, w, h, text, speed, s1, s2, o1, o2) => {
   }
 }
 
-export default class ParallaxScene extends Phaser.Scene {
+export default class GameScene extends Phaser.Scene {
   constructor() {
-    super('parallax-scene')
+    super('game-scene')
   }
 
   preload() {
@@ -23,6 +23,9 @@ export default class ParallaxScene extends Phaser.Scene {
     this.load.image('grass3', 'assets/images/grass3.png');
     this.load.image('ground', 'assets/images/ground.png');
     this.load.image('tree', 'assets/images/tree.png');
+    this.load.image('cloud1', 'assets/images/cloud1.png');
+    this.load.image('cloud2', 'assets/images/cloud2.png');
+    this.load.image('cloud3', 'assets/images/cloud3.png');
     this.load.image('rock1', 'assets/images/rock1.png');
     this.load.image('rock2', 'assets/images/rock2.png');
     this.load.image('rock3', 'assets/images/rock3.png');
@@ -39,38 +42,29 @@ export default class ParallaxScene extends Phaser.Scene {
     this.add.image(width * 0.5, height * 0.3, 'sky')
       .setScrollFactor(0).setScale(0.8, 0.7)
 
+    this.cloud1 = backgroundRepeat(this, 0, height * 0.45, 'cloud1', 0.15, 0.5, 0.5, 0, 1)
     this.mountain = backgroundRepeat(this, 0, height, 'mountain', 0.25, 0.5, 0.5, 0, 1)
-
+    this.cloud2 = backgroundRepeat(this, width / 2, height * 0.5, 'cloud2', 0.15, 0.5, 0.5, 0, 1)
     this.grass2 = backgroundRepeat(this, width / 2.4, height / 1.5, 'grass2', 0.5, 0.4, 0.4)
-
     this.grass1 = backgroundRepeat(this, width / 7.5, height / 1.5, 'grass3', 0.5, 0.4, 0.4)
-
     this.grass3 = backgroundRepeat(this, width / 1.3, height / 1.5, 'grass1', 0.5, 0.4, 0.4)
-
     this.ground = backgroundRepeat(this, 0, height / 1.1, 'ground', 0.75, 0.5, 0.5, 0, 1)
 
     this.tree1 = backgroundRepeat(this, width / 5, height / 1.8, 'tree', 0.75, 0.5, 0.5)
-
     this.tree2 = backgroundRepeat(this, width / 1.3, height / 1.6, 'tree', 0.75, 0.35, 0.35)
-
     this.rock1 = backgroundRepeat(this, width / 1.8, height / 1.3, 'rock2', 0.75, 0.4, 0.4)
-
     this.rock2 = backgroundRepeat(this, width / 3.5, height / 1.3, 'rock3', 0.75, 0.4, 0.4)
-
     this.rock3 = backgroundRepeat(this, width / 1.1, height / 1.3, 'rock1', 0.75, 0.4, 0.4)
-
     this.flower1 = backgroundRepeat(this, width / 1.7, height / 1.2, 'flower1', 0.75, 0.4, 0.4)
-
     this.flower2 = backgroundRepeat(this, width / 2.5, height / 1.3, 'flower2', 0.75, 0.4, 0.4)
-
     this.ground2 = backgroundRepeat(this, 0, height, 'ground2', 1.25, 0.45, 0.45, 0, 1)
 
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.cameras.main.setBounds(0, 0, width * 3, height)
+    this.cameras.main.setBounds(0, 0, width * 100, height)
   }
 
   update() {
-    const speed = 3
+    const speed = 10
     const cam = this.cameras.main
 
     if (this.cursors.left.isDown) {
