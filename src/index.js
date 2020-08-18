10982567da17a10b527f3e6fc2211209ebb9cab2
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-18 14:29:05 +0530

    Add phaser config file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('testing')
+import './styles/style.css';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: [HelloWorldScene]
+}
+
+export default new Phaser.Game(config)
