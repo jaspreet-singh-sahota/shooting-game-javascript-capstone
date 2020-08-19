1a54062160ac6552b9da39953bd389412e8a2e05
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-19 21:21:16 +0530

    Add enemy position function in enemy class

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -17,7 +17,15 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
     super(scene, x, y, 'enemy', 10);
   }
 
- 
+  enemyPosition(x, y) {
+    this.body.reset(x, y);
+    this.setActive(true);
+    this.setVisible(true);
+    this.flipX = true
+    this.setScale(1.4, 1.4)
+    this.anims.play('enemy')
+    this.body.enable = true
+  }
 }
 
 class LaserGroup extends Phaser.Physics.Arcade.Group {
