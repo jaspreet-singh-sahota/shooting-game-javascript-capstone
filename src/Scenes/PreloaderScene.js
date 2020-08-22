6166ea84c69758c7222ea5e927c2bc9e2581f9f1
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-22 05:45:50 +0530

    Fix animation frame bug

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -24,7 +24,7 @@ export default class PreloaderScene extends Phaser.Scene {
         key: 'logo',
         frames: this.anims.generateFrameNames('logo', {
           frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-            14, 15, 16, 17, 18, 19, 20, 21, 22],
+            14, 15, 16, 17, 18, 19, 20, 21],
         }),
         frameRate: 7.5,
         repeat: -1,
